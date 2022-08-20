9e858cbd9be8623d0dc5dbdab4d7c69264bf9fd7
Wellington Nogara <50090354+ElNogara@users.noreply.github.com>
2022-08-20 10:43:36 -0300

    Subindo modulo

---
+++ Elnogara_FirstModule/app/code/local/Elnogara/FirstModule/controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+class Elnogara_FirstModule_TestController extends Mage_Core_Controller_Front_Action
+{
+    public function testAction()
+    {
+?>
+        <h1>Hello World</h1>
+        <h5>Seu primeiro módulo em Magento 1</h5>
+<?php
+
+    }
+}
+?>
